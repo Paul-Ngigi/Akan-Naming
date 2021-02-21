3c987649a39849ea91b14c0e51a5124836f89693
Paul-Ngigi <paulkush7777@gmail.com>
2021-02-21 18:42:08 +0300

    add control flow for naming the users

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.addEventListener("DOMContentLoaded",function(){
     console.log("Content loaded");
-
+    let nameList = new Array();
     const dateForm = document.querySelector("#dateForm");
     const nameTbody = document.querySelector("#nameTbody");
     // console.log("Form added",dateForm);
@@ -16,17 +16,35 @@ window.addEventListener("DOMContentLoaded",function(){
         let dateInput = document.querySelector("#date").value;
         let dayofBirth = new Date(dateInput);
         dobDay = dayofBirth.getDay();
-        dateInput = dobDay;
-        // console.log(dateInput);
+        // dateInput = dobDay;
+        // console.log(dobDay);
 
         let nameRow = document.createElement("tr");
-        let nameData = document.createElement("td");
+        // let nameData = document.createElement("td");
 
-        nameRow.appendChild(nameData);
-
+        // nameRow.appendChild(nameData);
+        namesData = document.createElement("td");
         nameTbody.appendChild(nameRow);
 
         let maleGender = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
         let femaleGender = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+
+        if (gender == "Male") { 
+            nameList.push(maleGender[dobDay]);
+            displayName();
+        } else if(gender == "Female") {
+            nameList.push(femaleGender[dobDay]);
+            displayName();
+        } else { 
+            alert("Error chose male or femlae");
+        }
+
+        function displayName() { 
+            nameList.forEach(function (name) {
+                namesData.textContent = name;
+                nameRow.appendChild(namesData);
+                nameTbody.appendChild(nameRow);
+            });
+        }
     });
 });
